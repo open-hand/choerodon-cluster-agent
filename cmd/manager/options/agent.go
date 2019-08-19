baa5f4db55c08a9f1ac4fbc6de788f891c38c656
朱智阳 <zhiyang.zhu@hand-china.com>
2019-08-19 20:34:24 +0800

    [IMP] modify configMap

--- cmd/manager/options/agent.go
+++ cmd/manager/options/agent.go
@@ -24,6 +24,7 @@ import (
 	"github.com/spf13/pflag"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8sclient "k8s.io/client-go/kubernetes"
+	"strconv"
 	cmdutil "k8s.io/kubernetes/pkg/kubectl/cmd/util"
 	"net/http"
 	"os"
