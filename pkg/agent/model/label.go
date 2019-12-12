e2a6743e54a94d56ebcd2faf1c9bf5e9f3dd8ec9
李浩 <310103270@qq.com>
2019-12-12 16:10:20 +0800

    [FIX] 删除自定义资源的PV

--- pkg/agent/model/label.go
+++ pkg/agent/model/label.go
@@ -1,16 +1,17 @@
 package model
 
 const (
-	AppLabel            = "choerodon.io/application"
-	MicroServiceConfig  = "choerodon.io/feature"
-	AppVersionLabel     = "choerodon.io/version"
-	ReleaseLabel        = "choerodon.io/release"
-	NetworkLabel        = "choerodon.io/network"
-	NetworkNoDelLabel   = "choerodon.io/no_delete"
-	AgentVersionLabel   = "choerodon.io"
-	CommitLabel         = "choerodon.io/commit"
+	AppLabel           = "choerodon.io/application"
+	MicroServiceConfig = "choerodon.io/feature"
+	AppVersionLabel    = "choerodon.io/version"
+	ReleaseLabel       = "choerodon.io/release"
+	NetworkLabel       = "choerodon.io/network"
+	NetworkNoDelLabel  = "choerodon.io/no_delete"
+	AgentVersionLabel  = "choerodon.io"
+	CommitLabel        = "choerodon.io/commit"
+	// 拼写错误，暂时不要更改
 	CommandLabel        = "choeroodn.io/command"
-	AppServiceIdLabel   = "choeroodn.io/app-service-id"
+	AppServiceIdLabel   = "choerodon.io/app-service-id"
 	TestLabel           = "choerodon.io/test"
 	EnvLabel            = "choerodon.io/env"
 	PvLabel             = "choerodon.io/pv"
