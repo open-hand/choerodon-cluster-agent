71d9954839c8ad0c2d995e7b598488763cb67cd6
lihao <lihao@appledeMacBook-Pro-2.local>
2020-03-31 15:32:40 +0800

    [IMP] 兼容不同版本的deployment

--- pkg/kube/client.go
+++ pkg/kube/client.go
@@ -34,8 +34,6 @@ import (
 	"github.com/choerodon/choerodon-cluster-agent/pkg/agent/model"
 )
 
-var ClusterId int32
-
 type Client interface {
 	DeleteJob(namespace string, name string) error
 	LogsForJob(namespace string, name string, jobLabel string) (string, string, error)
@@ -69,6 +67,8 @@ type Client interface {
 
 const testContainer string = "automation-test"
 
+var ClusterId int32
+var KubernetesVersion string
 var AgentVersion string
 var expectedResourceKind = []string{"Deployment", "ReplicaSet", "ReplicationController", "Job"}
 
@@ -644,181 +644,6 @@ func labelRepoObj(info *resource.Info, namespace, version string) (runtime.Objec
 	return obj, nil
 }
 
-func nestedLocalObjectReferences(obj map[string]interface{}, fields ...string) ([]core_v1.LocalObjectReference, bool, error) {
-	val, found, err := unstructured.NestedFieldNoCopy(obj, fields...)
-	if !found || err != nil {
-		return nil, found, err
-	}
-
-	m, ok := val.([]core_v1.LocalObjectReference)
-	if ok {
-		return m, true, nil
-		//return nil, false, fmt.Errorf("%v accessor error: %v is of the type %T, expected []core_v1.LocalObjectReference", strings.Join(fields, "."), val, val)
-	}
-
-	if m, ok := val.([]interface{}); ok {
-		secrets := make([]core_v1.LocalObjectReference, 0)
-		for _, v := range m {
-			if v1, ok := v.(map[string]interface{}); ok {
-				v2 := v1["name"]
-				secret := core_v1.LocalObjectReference{}
-				if secret.Name, ok = v2.(string); ok {
-					secrets = append(secrets, secret)
-				}
-			}
-		}
-		return secrets, true, nil
-	}
-	return m, true, nil
-}
-
-func getTemplateLabels(obj map[string]interface{}) map[string]string {
-	tplLabels, _, err := unstructured.NestedStringMap(obj, "spec", "template", "metadata", "labels")
-	if err != nil {
-		glog.Warningf("Get Template Labels failed, %v", err)
-	}
-	if tplLabels == nil {
-		tplLabels = make(map[string]string)
-	}
-	return tplLabels
-}
-
-func setTemplateLabels(obj map[string]interface{}, templateLabels map[string]string) error {
-	return unstructured.SetNestedStringMap(obj, templateLabels, "spec", "template", "metadata", "labels")
-}
-
-// Provide a common method for adding labels
-func addLabel(imagePullSecret []core_v1.LocalObjectReference,
-	command int,
-	appServiceId int64,
-	info *resource.Info, version, releaseName, app, testLabel string, isTest bool) (runtime.Object, error) {
-	t := info.Object.(*unstructured.Unstructured)
-
-	l := t.GetLabels()
-	defer t.SetLabels(l)
-
-	if l == nil {
-		l = make(map[string]string)
-	}
-
-	var addBaseLabels = func() {
-		l[model.ReleaseLabel] = releaseName
-		l[model.AgentVersionLabel] = AgentVersion
-	}
-	var addAppLabels = func() {
-		l[model.AppLabel] = app
-		l[model.AppVersionLabel] = version
-	}
-
-	var addTemplateAppLabels = func() {
-		tplLabels := getTemplateLabels(t.Object)
-		tplLabels[model.ReleaseLabel] = releaseName
-		tplLabels[model.AgentVersionLabel] = AgentVersion
-		//12.05 新增打标签。
-		//0 表示的是安装未填入值 -1代表更新
-		if appServiceId != 0 && appServiceId != -1 {
-			tplLabels[model.AppServiceIdLabel] = strconv.FormatInt(int64(appServiceId), 10)
-		}
-		if !isTest {
-			tplLabels[model.CommandLabel] = strconv.Itoa(command)
-		}
-		tplLabels[model.AppLabel] = app
-		tplLabels[model.AppVersionLabel] = version
-		if err := setTemplateLabels(t.Object, tplLabels); err != nil {
-			glog.Warningf("Set Template Labels failed, %v", err)
-		}
-	}
-	var addSelectorAppLabels = func() {
-		selectorLabels, _, err := unstructured.NestedStringMap(t.Object, "spec", "selector", "matchLabels")
-		if err != nil {
-			glog.Warningf("Get Selector Labels failed, %v", err)
-		}
-		if selectorLabels == nil {
-			selectorLabels = make(map[string]string)
-		}
-		selectorLabels[model.ReleaseLabel] = releaseName
-		if err := unstructured.SetNestedStringMap(t.Object, selectorLabels, "spec", "selector", "matchLabels"); err != nil {
-			glog.Warningf("Set Selector label failed, %v", err)
-		}
-	}
-
-	// add private image pull secrets
-	var addImagePullSecrets = func() {
-		secrets, _, err := nestedLocalObjectReferences(t.Object, "spec", "template", "spec", "imagePullSecrets")
-		if err != nil {
-			glog.Warningf("Get ImagePullSecrets failed, %v", err)
-		}
-		if secrets == nil {
-			secrets = make([]core_v1.LocalObjectReference, 0)
-
-		}
-		secrets = append(secrets, imagePullSecret...)
-		// SetNestedField method just support a few types
-		s := make([]interface{}, 0)
-		for _, secret := range secrets {
-			m := make(map[string]interface{})
-			m["name"] = secret.Name
-			s = append(s, m)
-		}
-		if err := unstructured.SetNestedField(t.Object, s, "spec", "template", "spec", "imagePullSecrets"); err != nil {
-			glog.Warningf("Set ImagePullSecrets failed, %v", err)
-		}
-	}
-
-	kind := info.Mapping.GroupVersionKind.Kind
-	switch kind {
-	case "ReplicationController", "ReplicaSet", "Deployment":
-		addAppLabels()
-		addTemplateAppLabels()
-		addSelectorAppLabels()
-		addImagePullSecrets()
-	case "ConfigMap":
-	case "Service":
-		l[model.NetworkLabel] = "service"
-		l[model.NetworkNoDelLabel] = "true"
-	case "Ingress":
-		l[model.NetworkLabel] = "ingress"
-		l[model.NetworkNoDelLabel] = "true"
-	case "Job":
-		addImagePullSecrets()
-		if isTest {
-			l[model.TestLabel] = testLabel
-			tplLabels := getTemplateLabels(t.Object)
-			tplLabels[model.TestLabel] = testLabel
-			tplLabels[model.ReleaseLabel] = releaseName
-			if err := setTemplateLabels(t.Object, tplLabels); err != nil {
-				glog.Warningf("Set Test-Template Labels failed, %v", err)
-			}
-		}
-	case "DaemonSet", "StatefulSet":
-		addAppLabels()
-		addTemplateAppLabels()
-		addImagePullSecrets()
-	case "Secret":
-		addAppLabels()
-	case "RoleBinding", "ClusterRoleBinding", "Role", "ClusterRole", "PodSecurityPolicy", "ServiceAccount":
-	case "Pod":
-		addAppLabels()
-	default:
-		glog.Warningf("Add Choerodon label failed, unsupported object: Kind %s of Release %s", kind, releaseName)
-		return t, nil
-	}
-	// add base labels
-	addBaseLabels()
-	return t, nil
-}
-
-func labelObject(imagePullSecret []core_v1.LocalObjectReference,
-	command int,
-	info *resource.Info,
-	releaseName string,
-	app string,
-	version string,
-	appServiceId int64) (runtime.Object, error) {
-
-	return addLabel(imagePullSecret, command, appServiceId, info, version, releaseName, app, "", false)
-}
-
 func isFoundPod(podItem []core_v1.Pod, pod core_v1.Pod) bool {
 	for _, value := range podItem {
 		if (value.Namespace == pod.Namespace) && (value.Name == pod.Name) {
@@ -869,8 +694,6 @@ func getSelectorFromObject(obj runtime.Object) (map[string]string, bool) {
 		return nil, false
 	}
 }
-
-
 
 func (c *client) CreateOrUpdateDockerRegistrySecret(namespace string, secret *core_v1.Secret) (*core_v1.Secret, error) {
 	_, err := c.client.CoreV1().Secrets(namespace).Get(secret.Name, meta_v1.GetOptions{})
