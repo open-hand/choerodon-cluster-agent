98e2a2f3b579825415b7a87d2e92a6fef0609e2c
lihao <310103270@qq.com>
2020-07-07 17:49:41 +0800

    [IMP] 修复chart用户名和密码添加逻辑

--- pkg/command/helm/common.go
+++ pkg/command/helm/common.go
@@ -2,19 +2,27 @@ package helm
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/choerodon/choerodon-cluster-agent/pkg/agent/model"
 	"github.com/choerodon/choerodon-cluster-agent/pkg/kube"
 	"github.com/choerodon/choerodon-cluster-agent/pkg/util/command"
 	"github.com/choerodon/choerodon-cluster-agent/pkg/util/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"net/url"
 )
 
 func GetCharUsernameAndPassword(opts *command.Opts, cmd *model.Packet) (string, string, error) {
-	payload := make(map[string]string)
+	payload := make(map[string]interface{})
 	if err := json.Unmarshal([]byte(cmd.Payload), &payload); err != nil {
 		return "", "", err
 	}
-	repoUrl := payload["repoURL"]
+
+	repo, err := url.Parse(payload["repoURL"].(string))
+	if err != nil {
+		return "", "", err
+	}
+
+	repoHost := fmt.Sprintf("%s://%s", repo.Scheme, repo.Host)
 
 	secret, err := opts.KubeClient.GetKubeClient().CoreV1().Secrets(kube.AgentNamespace).Get(SecretName, metav1.GetOptions{})
 	if err != nil {
@@ -29,7 +37,7 @@ func GetCharUsernameAndPassword(opts *command.Opts, cmd *model.Packet) (string, 
 	if err := json.Unmarshal(secret.Data["info"], &chartAccountMap); err != nil {
 		return "", "", err
 	}
-	if chartAccount, ok := chartAccountMap[repoUrl]; ok {
+	if chartAccount, ok := chartAccountMap[repoHost]; ok {
 		return chartAccount.Username, chartAccount.Password, nil
 	}
 
