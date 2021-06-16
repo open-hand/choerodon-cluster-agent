a4feeb240d6acc1092e9b08f8ec6d7a6a8fb8c41
lihao <310103270@qq.com>
2021-06-16 09:45:42 +0800

    [ADD] 支持StatefulSet的scale功能

--- pkg/command/kubernetes/scale.go
+++ pkg/command/kubernetes/scale.go
@@ -4,13 +4,15 @@ import (
 	"encoding/json"
 	"github.com/choerodon/choerodon-cluster-agent/pkg/agent/model"
 	"github.com/choerodon/choerodon-cluster-agent/pkg/util/command"
+	"github.com/pkg/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type ScalePodRequest struct {
-	DeploymentName string `json:"deploymentName,omitempty"`
-	Count          int    `json:"count,omitempty"`
-	Namespace      string `json:"namespace,omitempty"`
+	Name      string `json:"name,omitempty"`
+	Count     int    `json:"count,omitempty"`
+	Namespace string `json:"namespace,omitempty"`
+	Kind      string `json:"kind,omitempty"`
 }
 
 func ScalePod(opts *command.Opts, cmd *model.Packet) ([]*model.Packet, *model.Packet) {
@@ -19,20 +21,37 @@ func ScalePod(opts *command.Opts, cmd *model.Packet) ([]*model.Packet, *model.Pa
 	if err != nil {
 		return nil, command.NewResponseError(cmd.Key, model.OperatePodCountFailed, err)
 	}
+	switch req.Kind {
+	case "Deployment":
+		clientSet := opts.KubeClient.GetKubeClient()
+		s, err := clientSet.AppsV1().Deployments(req.Namespace).GetScale(req.Name, v1.GetOptions{})
+		if err != nil {
+			return nil, command.NewResponseError(cmd.Key, model.OperatePodCountFailed, err)
+		}
 
-	clientSet := opts.KubeClient.GetKubeClient()
-	s, err := clientSet.AppsV1().Deployments(req.Namespace).GetScale(req.DeploymentName, v1.GetOptions{})
-	if err != nil {
-		return nil, command.NewResponseError(cmd.Key, model.OperatePodCountFailed, err)
+		s.Spec.Replicas = int32(req.Count)
+
+		_, err = clientSet.AppsV1().Deployments(req.Namespace).UpdateScale(req.Name, s)
+		if err != nil {
+			return nil, command.NewResponseError(cmd.Key, model.OperatePodCountFailed, err)
+		}
+	case "StatefulSet":
+		clientSet := opts.KubeClient.GetKubeClient()
+		s, err := clientSet.AppsV1().StatefulSets(req.Namespace).GetScale(req.Name, v1.GetOptions{})
+		if err != nil {
+			return nil, command.NewResponseError(cmd.Key, model.OperatePodCountFailed, err)
+		}
+
+		s.Spec.Replicas = int32(req.Count)
+
+		_, err = clientSet.AppsV1().StatefulSets(req.Namespace).UpdateScale(req.Name, s)
+		if err != nil {
+			return nil, command.NewResponseError(cmd.Key, model.OperatePodCountFailed, err)
+		}
+	default:
+		return nil, command.NewResponseError(cmd.Key, model.OperatePodCountFailed, errors.New("unsupported resource kind"))
+
 	}
-
-	s.Spec.Replicas = int32(req.Count)
-
-	_, err = clientSet.AppsV1().Deployments(req.Namespace).UpdateScale(req.DeploymentName, s)
-	if err != nil {
-		return nil, command.NewResponseError(cmd.Key, model.OperatePodCountFailed, err)
-	}
-
 	resp := &model.Packet{
 		Key:  cmd.Key,
 		Type: model.OperatePodCountSuccess,
